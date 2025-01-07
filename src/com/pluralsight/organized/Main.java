dbe009c18c46c879a10fac4861c07e6a224c1357
Roman Ismagilov <ismagilov771@gmail.com>
2025-01-07 13:07:13 +0300

    init

---
+++ src/com/pluralsight/organized/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight.organized;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+
+        taskManager.addTask("clean the room");
+        taskManager.addTask("buy groceries");
+        taskManager.addTask("jogging");
+        taskManager.addTask("call to bank");
+
+        taskManager.removeTask(3);
+        taskManager.removeTask(50);
+
+        taskManager.editTask(1, "get cash");
+        taskManager.editTask(40, "get gas");
+
+        taskManager.markTaskAsCompleted(1);
+        taskManager.markTaskAsCompleted(60);
+
+        taskManager.showTasks();
+    }
+}
---
+++ src/com/pluralsight/organized/TaskManager.java
@@ -0,0 +1,72 @@
+package com.pluralsight.organized;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    List<String> tasks = new ArrayList<>();
+    List<Boolean> isCompleted = new ArrayList<>();
+
+    public void addTask(String task) {
+        tasks.add(task);
+        isCompleted.add(false);
+    }
+
+    public void removeTask(int index) {
+        if (isIndexInvalid(index)) {
+            System.out.println("Invalid index when remove: " + index);
+            return;
+        }
+
+        tasks.remove(index);
+        isCompleted.remove(index);
+    }
+
+    public void markTaskAsCompleted(int index) {
+        if (isIndexInvalid(index)) {
+            System.out.println("Invalid index when mark task as completed: " + index);
+            return;
+        }
+
+        isCompleted.set(index, true);
+    }
+
+    public void editTask(int index, String newTask) {
+        if (isIndexInvalid(index)) {
+            System.out.println("Invalid index when edit: " + index);
+            return;
+        }
+
+        tasks.set(index, newTask);
+    }
+
+    public void showTasks() {
+        String report = generateReport();
+        System.out.println(report);
+    }
+
+    private boolean isIndexInvalid(int index) {
+        return index < 0 || index >= tasks.size();
+    }
+
+    private String generateReport() {
+        if (tasks.isEmpty()) {
+            return "Список задач пуст";
+        }
+
+        StringBuilder result = new StringBuilder("=== Список задач ===\n");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            StringBuilder innerSb = new StringBuilder();
+            int orderNum = i + 1;
+            String status = isCompleted.get(i) ? "[x]" : "[ ]";
+
+            innerSb.append(orderNum).append(". ").append(status).append(" ").append(tasks.get(i)).append("\n");
+
+            result.append(innerSb);
+        }
+        result.append("=== Конец отчёта ===");
+
+        return result.toString();
+    }
+}
